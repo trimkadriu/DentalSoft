01ded07bd6a8a0fcc28d90a4ca841da438b6274f
trimkadriu <jareck_8@hotmail.com>
2013-08-24 00:20:50 +0200

    Added login form

--- DentalSoft/Program.cs
+++ DentalSoft/Program.cs
@@ -16,7 +16,7 @@ namespace DentalSoft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
