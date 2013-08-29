3f1424bdf11ed4f8a31c70f570e0c350534f924f
Trim Kadriu <trimkadriu@users.noreply.github.com>
2013-08-29 17:06:50 +0200

    Changes from work PC

---
+++ DentalSoft.Repositories/Connection.cs
@@ -0,0 +1,86 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Dentalsoft.Repositories
+{
+    private abstract class Connection
+    {
+        protected MySqlConnection connection;
+        private string server;
+        private string database;
+        private string username;
+        private string password;
+
+        public Connection()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "dentalsoft";
+            username = "dentalsoft";
+            password = "dentalsoft";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        protected bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Nuk mund te kyqet ne bazen e te dhenave.", "Gabim", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Qasja ne bazen e te dhenave nuk eshte ne rregull.", "Gabim", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+
+                    default:
+                        MessageBox.Show("MySQL numri i gabimit: " + ex.Number, "Gabim", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+                return false;
+            }
+        }
+
+        protected bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        protected void testConnection()
+        {
+            if (OpenConnection())
+                MessageBox.Show("Lidhja ne rregull");
+            else
+                MessageBox.Show("Lidhja nuk eshte ne rregull");
+        }
+
+        private abstract void insertStatement();
+        private abstract void updateStatement();
+        private abstract void deleteStatement();
+        private abstract List<object>[] selectStatement();
+    }
+}
---
+++ DentalSoft.Repositories/DentistsRepository.cs
@@ -0,0 +1,68 @@
+﻿using Dentalsoft.Repositories;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace DentalSoft.Repositories
+{
+    class DentistsRepository : Connection
+    {
+        string query;
+
+        public DentistsRepository()
+        {
+            
+        }
+
+        override public void insertStatement()
+        {
+            if (base.OpenConnection())
+            {
+                query = "";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        override public void updateStatement()
+        {
+            if (base.OpenConnection())
+            {
+                query = "";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        override public void deleteStatement()
+        {
+            if (base.OpenConnection())
+            {
+                query = "";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        override public List<object>[] selectStatement()
+        {
+            if (base.OpenConnection())
+            {
+                query = "";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+
+                }
+                dataReader.Close();
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+                return null;
+            }
+            return null;
+        }
+    }
+}
